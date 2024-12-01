1868b598d9934fea41b3acb5da8f49601cec4ebb
Sanidhya Gupta <guptasanidhya6@gmail.com>
2024-12-01 16:57:49 +0530

    adding qualifier annotation so taht autowiring can be done by name

--- spring_durgesh/springcore/src/main/java/com/springcore/auto/wire/annotation/Emp.java
+++ spring_durgesh/springcore/src/main/java/com/springcore/auto/wire/annotation/Emp.java
@@ -1,6 +1,7 @@
 package com.springcore.auto.wire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Emp {
 
@@ -26,6 +27,7 @@ public class Emp {
     }
 
     @Autowired
+    @Qualifier("address2")
     public void setAddress(Address address) {
         this.address = address;
     }
