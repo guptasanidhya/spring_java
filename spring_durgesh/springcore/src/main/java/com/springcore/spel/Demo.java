24734261d79ad0e0892e39ad98c62e383d913dff
Sanidhya Gupta <guptasanidhya6@gmail.com>
2024-12-05 10:25:01 +0530

    using spel to add class and variables

--- spring_durgesh/springcore/src/main/java/com/springcore/spel/Demo.java
+++ spring_durgesh/springcore/src/main/java/com/springcore/spel/Demo.java
@@ -13,6 +13,26 @@ public class Demo {
     @Value("#{44}")
     private int y;
 
+    @Value("#{T(java.lang.Math).sqrt(144)}")
+    private double z;
+
+    @Value("#{T(java.lang.Math).E}")
+    private double e;
+
+    @Value("#{T(java.lang.Math).PI}")
+    private double pi;
+
+    @Value("#{new java.lang.String('Sanidhya')}")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getX() {
         return x;
     }
@@ -29,14 +49,39 @@ public class Demo {
         this.y = y;
     }
 
-
-
     @Override
     public String toString() {
         return "Demo{" +
                 "x=" + x +
                 ", y=" + y +
+                ", z=" + z +
+                ", e=" + e +
+                ", pi=" + pi +
+                ", name='" + name + '\'' +
                 '}';
     }
 
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public double getE() {
+        return e;
+    }
+
+    public void setE(double e) {
+        this.e = e;
+    }
+
+    public double getPi() {
+        return pi;
+    }
+
+    public void setPie(double pi) {
+        this.pi = pi;
+    }
 }
