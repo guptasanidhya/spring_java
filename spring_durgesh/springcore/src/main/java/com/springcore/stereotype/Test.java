afc9b6370dd12e5300a4e61b7cf29851c4e24d70
Sanidhya Gupta <guptasanidhya6@gmail.com>
2024-12-05 09:36:02 +0530

    adding scope of bean using annotation and xml

--- spring_durgesh/springcore/src/main/java/com/springcore/stereotype/Test.java
+++ spring_durgesh/springcore/src/main/java/com/springcore/stereotype/Test.java
@@ -3,6 +3,8 @@ package com.springcore.stereotype;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.awt.*;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -10,5 +12,11 @@ public class Test {
         Student student = context.getBean("student", Student.class);
         System.out.println(student);
         System.out.println(student.getAddress());
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        Teacher teacher1 = context.getBean("teacher",Teacher.class);
+        System.out.println(teacher1.hashCode());
+        Teacher teacher2 = context.getBean("teacher",Teacher.class);
+        System.out.println(teacher2.hashCode());
+
     }
 }
