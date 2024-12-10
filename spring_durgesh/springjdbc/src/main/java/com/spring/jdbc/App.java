c94e0c7c7494e63cb243e11267e0a72e4b0f7df1
Sanidhya Gupta <guptasanidhya6@gmail.com>
2024-12-10 06:54:04 +0530

    Adding row mapper interface and implementation for select query

--- spring_durgesh/springjdbc/src/main/java/com/spring/jdbc/App.java
+++ spring_durgesh/springjdbc/src/main/java/com/spring/jdbc/App.java
@@ -5,6 +5,9 @@ import com.spring.jdbc.entities.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -17,12 +20,31 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 
         StudentDaoImpl studentDao = context.getBean("studentDao",StudentDaoImpl.class);
-        Student student = new Student();
-        student.setId(1);
-        student.setName("Sundy");
-        student.setCity("Indore");
+//        Student student = new Student();
+//        student.setId(1);
+//        student.setName("Sundy");
+//        student.setCity("Indore");
+//        int result = studentDao.insert(student);
+//        System.out.println("Row inserted.. "+result);
 
-        int result = studentDao.insert(student);
-        System.out.println("Row inserted.. "+result);
+
+//        Student student = new Student();
+//        student.setId(1);
+//        student.setName("Deepak");
+//        student.setCity("Dewas");
+//
+//        int result = studentDao.change(student);
+//        System.out.println("rows affected.." + result);
+
+//        int result = studentDao.delete(1);
+//        System.out.println("Student deleted" + result);
+
+//        Student student = studentDao.getStudent(222);
+//        System.out.println(student);
+
+        List<Student> students = studentDao.getAllStudents();
+        for(Student s : students){
+            System.out.println(s);
+        }
     }
 }
--- spring_durgesh/springjdbc/src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
+++ spring_durgesh/springjdbc/src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
@@ -2,6 +2,9 @@ package com.spring.jdbc.dao;
 
 import com.spring.jdbc.entities.Student;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
 
 public class StudentDaoImpl implements  StudentDao{
     private JdbcTemplate jdbcTemplate;
@@ -21,5 +24,35 @@ public class StudentDaoImpl implements  StudentDao{
         String query = "insert into student values(?,?,?)";
         int r = this.jdbcTemplate.update(query,student.getId(),student.getName(),student.getCity());
         return r;
+    }
+
+    @Override
+    public int change(Student student) {
+        String query ="update student set name=? , city= ? where id=?";
+        int r = this.jdbcTemplate.update(query,student.getName(),student.getCity(),student.getId());
+        return r;
+    }
+
+    @Override
+    public int delete(int studentId) {
+        String query = "delete from student where id=?";
+        int r = this.jdbcTemplate.update(query,studentId);
+        return r;
+    }
+
+    @Override
+    public Student getStudent(int studentId) {
+        String query = "Select * from student where Id=?";
+        RowMapper<Student> rowMapper = new RowMapperImpl();
+        Student student=this.jdbcTemplate.queryForObject(query,rowMapper,studentId);
+        return student;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        String query="Select * from student";
+        RowMapper<Student> rowMapper = new RowMapperImpl();
+        List<Student> students = this.jdbcTemplate.query(query,rowMapper);
+        return students;
     }
 }
